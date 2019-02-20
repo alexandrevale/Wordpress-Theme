10aeea56789e304ee86c89d8580056b19403588e
Alexandre do Vale <vale.apv@gmail.com>
2019-02-20 08:06:48 -0300

    Atualização no functions.php com as requisições CSS e JS

--- footer.php
+++ footer.php
@@ -1,43 +1,6 @@
-	<footer>
-		<div class="container-fluid">
-			<div class="row wrapper">
-				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 redes_sociais">
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0">
-						<a href="" title="Facebook da Uro Clínica">
-							<img src="<?php bloginfo('template_url') ?>/imgs/icones/footer_facebook.png" alt="" class="element_center">	
-						</a>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0">
-						<a href="" title="Instagram da Uro Clínica">
-							<img src="<?php bloginfo('template_url') ?>/imgs/icones/footer_instagram.png" alt="" class="element_center">
-						</a>
-					</div>
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<img src="<?php bloginfo('template_url') ?>/imgs/icones/footer_logo.png" alt="" class="element_center">
-				</div>
-				<div class="col-lg-3 col-md-2 col-sm-2 col-xs-12 summer">
-					<a href="http://summercomunicacao.com.br" title="Summer Comunicação">
-						<img src="<?php bloginfo('template_url') ?>/imgs/logos/summer_comunicacao.png" alt="" class="img-responsive">
-					</a>
-					<p>Todos os direitos reservados - 2018</p>
-				</div>
-			</div>
-		</div>
+	<footer class="blog-footer">
+
 	</footer>
-	<div class="footer_detalhe"></div>
-	<!-- JS -->
-	<script src="<?php bloginfo('template_url');?>/js/vendor/jquery-3.3.1.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/vendor/bootstrap.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/vendor/owl.carousel.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/vendor/jquery.mask.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/vendor/jquery.validate.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/vendor/lightbox.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/form.js"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCsWaUlbcEEGfID_kGoQULr8E2pLd60jlE&language=pt-br&visualization&callback=initMap"></script>
-    <script src="<?php bloginfo('template_url');?>/js/mapa.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/mask.js"></script>
-    <script src="<?php bloginfo('template_url');?>/js/functions.js"></script>
 	<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -12,25 +12,18 @@
   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
   remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-  // Habilita a opção de thumbnails (Imagem destacada) dos Posts / Páginas
   add_theme_support( 'post-thumbnails' );
-  //parâmetros (width, height, recortar imagem para que adapte ao padrão de w e h)
   set_post_thumbnail_size( 1280, 720, true );
 
   function carregar_css_js() {  
     
-    // Carregando CSS header
+    wp_enqueue_style('bootstrap_css', get_stylesheet_uri().'/src/css/bootstrap.css');
+    wp_enqueue_style('style_css', get_stylesheet_uri());
 
-    /*
-    <script src="<?php bloginfo('template_url') ?>/js/jquery.js"></script>
-    <script src="<?php bloginfo('template_url') ?>/js/popper.js"></script>
-    <script src="<?php bloginfo('template_url') ?>/js/bootstrap.js"></script>
-    */ 
-    wp_enqueue_style('style', get_stylesheet_uri() );
-    wp_enqueue_script('jquery', get_template_directory_uri().'/js/jquery.js');
-    wp_enqueue_script('popper', get_template_directory_uri().'/js/popper.js');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js');
-    wp_enqueue_script('scripts', get_template_directory_uri().'/js/scripts.js');
+    wp_enqueue_script('jquery', get_template_directory_uri().'/src/js/jquery.js');
+    wp_enqueue_script('popper', get_template_directory_uri().'/src/js/popper.js');
+    wp_enqueue_script('bootstrap_js', get_template_directory_uri().'/src/js/bootstrap.js', array('jquery'), '', true;
+    wp_enqueue_script('scripts', get_template_directory_uri().'/src/js/index.js', array('jquery'), '', true);
 
   }
   add_action( 'wp_enqueue_scripts', 'carregar_css_js' );
